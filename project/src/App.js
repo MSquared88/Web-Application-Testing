52f6b6d485a02c970651b59602f9ef42b17a7a2c
Matthew Meeves <weatherguy75@yahoo.com>
2019-09-05 16:16:51 -0500

    added functionallity

--- project/src/App.js
+++ project/src/App.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [strikes, setStrikes] = useState(0)
+  const [balls, setBalls] = useState(0)
+  const reset = () => {
+    setBalls(0)
+    setStrikes(0)
+  }
+  
+  const addStrike = () => {
+    const sum = strikes + 1
+    if(sum >= 3){
+      reset()
+    }else{setStrikes(sum)}
+  }
+
+  const addBall = () => {
+    const sum = balls + 1
+    if(sum >= 4){
+      reset()
+    }else{setBalls(sum)}
+    
+  }
+  
+
+  
+
   return (
     <div className="App">
+      <h1>Baseball App</h1>
+      <h2>Strikes: {strikes}</h2>
+      <h2>Balls: {balls}</h2>
+      <div className= 'btn=container'>
+        <button onClick= {addStrike} >Strike</button>
+        <button onClick= {addBall} >Ball</button>
+        <button onClick= {addStrike} >Foul</button>
+        <button onClick= {reset} >Hit</button>
+      </div>
 
     </div>
   );
 }
 
 export default App;
+function newFunction(strikes) {
+  return strikes + 1;
+}
+
