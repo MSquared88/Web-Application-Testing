ae9ff4591e28fc02a67f0172381e37752f4bf009
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-09-05 20:09:36 -0500

    added tests

--- project/src/App.js
+++ project/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   const [strikes, setStrikes] = useState(0)
   const [balls, setBalls] = useState(0)
+
+  
   const reset = () => {
     setBalls(0)
     setStrikes(0)
@@ -11,7 +13,7 @@ function App() {
   
   const addStrike = () => {
     const sum = strikes + 1
-    sum >= 4 ? reset() : setStrikes(sum) 
+    sum >= 3 ? reset() : setStrikes(sum) 
 
   }
 
@@ -24,11 +26,11 @@ function App() {
     return (
       <div className="App">
         <h1>Baseball App</h1>
-        <h2>Strikes: {strikes}</h2>
-        <h2>Balls: {balls}</h2>
+        <h2 data-testid="strikevalue">Strikes: {strikes}</h2>
+        <h2 data-testid="ballsvalue">Balls: {balls}</h2>
         <div className= 'btn=container'>
-          <button onClick= {addStrike} >Strike</button>
-          <button onClick= {addBall} >Ball</button>
+          <button onClick= {addStrike} data-testid="strikeBtn" >Strike</button>
+          <button onClick= {addBall} data-testid="ballBtn">Ball</button>
           <button onClick= {addStrike} >Foul</button>
           <button onClick= {reset} >Hit</button>
         </div>
--- project/src/App.test.js
+++ project/src/App.test.js
@@ -2,8 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { render, getByTestId, fireEvent } from '@testing-library/react'
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test("app loads with state of 0", () =>{
+  const { container } = render(<App/>);
+  const strikeValue = getByTestId(container, 'strikevalue')
+  const ballsValue = getByTestId(container, 'ballsvalue')
+
+  expect(strikeValue.textContent).toBe('Strikes: 0')
+  expect(ballsValue.textContent).toBe('Balls: 0')
+
+})
+
+test('strike buttons work and that values reset', () => {
+  const { container } = render(<App/>);
+  const strikeValue = getByTestId(container, 'strikevalue')
+
+  const strike = getByTestId(container, "strikeBtn");
+
+  expect(strikeValue.textContent).toBe("Strikes: 0");
+  fireEvent.click(strike);
+  expect(strikeValue.textContent).toBe("Strikes: 1");
+  fireEvent.click(strike);
+  expect(strikeValue.textContent).toBe("Strikes: 2");
+  fireEvent.click(strike);
+  expect(strikeValue.textContent).toBe("Strikes: 0");
+})
+
+test('ball buttons work and that values reset', () => {
+  const { container } = render(<App/>);
+  const ballsValue = getByTestId(container, 'ballsvalue')
+
+  const ball = getByTestId(container, "ballBtn");
+
+  expect(ballsValue.textContent).toBe("Balls: 0");
+  fireEvent.click(ball);
+
+  expect(ballsValue.textContent).toBe("Balls: 1");
+  fireEvent.click(ball);
+
+  expect(ballsValue.textContent).toBe("Balls: 2");
+  fireEvent.click(ball);
+
+  expect(ballsValue.textContent).toBe("Balls: 3");
+  fireEvent.click(ball);
+
+  expect(ballsValue.textContent).toBe("Balls: 0");
+})
